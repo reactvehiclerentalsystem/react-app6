9570dfbc64d5898d8af431d7981d4f1d9c6ac15e
Shubham Raj <SINGH29199810@GMAIL.COM>
2021-06-07 11:24:22 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Nav, Navbar } from "react-bootstrap";
 import { AppNavBar } from "./common/AppNavBar";
 
 function App() {
-  //Updated By SRJ
+  //Updated
   return (
     <Router>
       <AppNavBar />
